292f977d3e35e1194260f08a72db0fb98b68c4da
Sam Wilcox <swilcox21@gmail.com>
2022-09-07 14:45:47 -0400

    cursive

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,20 @@
-import '../App.css';
-import { Link } from 'react-router-dom';
+import "../App.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="home-container">
-        <h1>You are my Home... </h1>
-        <br/>    
-        <br/>    
-        <br/>    
-        <br/>    
-        <h1>Forever</h1>
-        <Link to="/love-note">
-            <button className="love-note">Read Me</button>
-        </Link>
+      <h1>You are my Home... </h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Forever</h1>
+      <br />
+      <br />
+      <Link to="/love-note">
+        <button className="love-note-button">Read Me</button>
+      </Link>
     </div>
   );
 }
